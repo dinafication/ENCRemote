0cb669ee40811f7b805201b19e58a5e6178fdf60
Mladen Čikara <mladen.cikara@gmail.com>
2013-08-13 17:55:44 +0200

    dodao gašenje Http Clienta kada nije više potreban

--- encchecker-parent/encchecker-app/src/main/java/hr/hackweek/encchecker/fragments/StateFrag.java
+++ encchecker-parent/encchecker-app/src/main/java/hr/hackweek/encchecker/fragments/StateFrag.java
@@ -57,10 +57,12 @@ public class StateFrag extends Fragment {
 	private class DownloadEncStateTask extends AsyncTask<String, Void, Float> {
 
 		// Create a new HttpClient and Post Header
-		private AndroidHttpClient httpclient = AndroidHttpClient.newInstance("AndroidHttpClient");
+		private AndroidHttpClient httpclient;
 
 		@Override
 		protected void onPreExecute() {
+			httpclient = AndroidHttpClient.newInstance("AndroidHttpClient");
+			
 			pd = new ProgressDialog(view.getContext());
 			pd.setTitle("Processing...");
 			pd.setMessage("Please wait.");
@@ -87,6 +89,7 @@ public class StateFrag extends Fragment {
 		@Override
 		protected void onPostExecute(Float result) {
 			pd.dismiss();
+			httpclient.close();
 
 			TextView encState = (TextView) view.findViewById(R.id.enc_stanje_iznos);
 			encState.setText(result.toString());

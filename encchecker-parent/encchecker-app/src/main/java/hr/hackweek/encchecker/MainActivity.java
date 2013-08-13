d848d419ee9a7fb4f262f140e67e069d18fe986a
dinafication <BartSimpson!1>
2013-08-13 16:34:05 +0200

    meni

--- encchecker-parent/encchecker-app/src/main/java/hr/hackweek/encchecker/MainActivity.java
+++ encchecker-parent/encchecker-app/src/main/java/hr/hackweek/encchecker/MainActivity.java
@@ -2,46 +2,69 @@ package hr.hackweek.encchecker;
 
 import hr.hackweek.encchecker.fragments.PasswordFrag;
 import hr.hackweek.encchecker.fragments.StateFrag;
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.util.Log;
 import android.view.Menu;
+import android.view.ViewGroup;
 
 public class MainActivity extends FragmentActivity {
+	
+	private StateFrag stateFrag;
+	private PasswordFrag passwordFrag;
 
-	/**
-	 * Called when the activity is first created.
-	 * 
-	 * @param savedInstanceState
-	 *            If the activity is being re-initialized after previously being
-	 *            shut down then this Bundle contains the data it most recently
-	 *            supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it
-	 *            is null.</b>
-	 */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		stateFrag = new StateFrag();
+		setFragment(stateFrag, R.id.fragmentViewGroup, true);
+		
+		passwordFrag = new PasswordFrag();
+		
+	}
+
+	private void setFragment(Fragment f, int id, boolean add){
 		FragmentManager fragmentManager = getSupportFragmentManager();
 		FragmentTransaction fragmentTransaction = fragmentManager
 				.beginTransaction();
 
-		//StateFrag statfrag = new StateFrag();
-		PasswordFrag statfrag = new PasswordFrag();
-		fragmentTransaction.add(R.id.fragmentViewGroup, statfrag);
+		if(add)fragmentTransaction.add(id, f);
+		else fragmentTransaction.replace(id, f);
 		fragmentTransaction.commit();
 	}
+	
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(hr.hackweek.encchecker.R.menu.main, menu);
+		return true;
+	}
 
-//	@Override
-//	public boolean onCreateOptionsMenu(Menu menu) {
-//		// Inflate the menu; this adds items to the action bar if it is present.
-//		getMenuInflater().inflate(hr.hackweek.encchecker.R.menu.main, menu);
-//		return true;
-//	}
+	public boolean onOptionsItemSelected(android.view.MenuItem item) {
+		
+		switch (item.getItemId()) {
+		case R.id.settings1_postavke:
+			setFragment(new PasswordFrag(), R.id.fragmentViewGroup, false);
+			
+			//getLayoutInflater().inflate(R.layout.password_frag, (ViewGroup) findViewById(R.id.fragmentViewGroup), false);
+			break;
+		case R.id.settings2_stanje:
+			setFragment(new StateFrag(), R.id.fragmentViewGroup, false);
+			
+			//getLayoutInflater().inflate(R.layout.state_frag, (ViewGroup) findViewById(R.id.fragmentViewGroup), false);
+			break;
+		case R.id.settings3_pomoc:
+			//setFragment(stateFrag, R.id.fragmentViewGroup, false); // TODO
+			break;
+		}	
+		return false;
+		
+	}
 
 }

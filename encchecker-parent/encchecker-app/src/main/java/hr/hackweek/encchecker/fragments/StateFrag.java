1aa1c848379102d8a67a8cd8d4ed51aeff1768af
Mladen Čikara <mladen.cikara@gmail.com>
2013-08-18 15:55:06 +0200

    mala refaktoriranja

--- encchecker-parent/encchecker-app/src/main/java/hr/hackweek/encchecker/fragments/StateFrag.java
+++ encchecker-parent/encchecker-app/src/main/java/hr/hackweek/encchecker/fragments/StateFrag.java
@@ -23,7 +23,6 @@ import android.content.SharedPreferences.Editor;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.http.AndroidHttpClient;
-import android.opengl.Visibility;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -34,14 +33,9 @@ import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.view.animation.LayoutAnimationController;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
-import android.widget.TableLayout;
-import android.widget.TableRow;
 import android.widget.TextView;
-
-import android.view.animation.AnimationUtils;
 
 public class StateFrag extends Fragment {
 

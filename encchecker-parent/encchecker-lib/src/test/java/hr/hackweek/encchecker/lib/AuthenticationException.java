8b738ba5e1e2137c24b3650a2f37e2ffd9bbf671
dinafication <dinafication@gmail.com>
2013-10-27 14:48:44 +0100

    popravak

---
+++ encchecker-parent/encchecker-lib/src/test/java/hr/hackweek/encchecker/lib/AuthenticationException.java
@@ -0,0 +1,10 @@
+package hr.hackweek.encchecker.lib;
+
+public class AuthenticationException extends Exception {
+
+	private static final long serialVersionUID = -499547693459175036L;
+
+	public AuthenticationException(String s) {
+		super(s);
+	}
+}
